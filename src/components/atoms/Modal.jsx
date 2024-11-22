aa612f869d0b7ef71d0337fdfda0776778c3d009
maldikurniawan <akurniawan2102@gmail.com>
2024-11-22 13:33:35 +0700

    fix bug

--- src/components/atoms/Modal.jsx
+++ src/components/atoms/Modal.jsx
@@ -1,16 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 
 const Modal = ({ isOpen, onClose, children }) => {
   const modalRef = useRef(null);
 
+  // Menambahkan dan menghapus pointer-events pada body
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.pointerEvents = 'none';
+    } else {
+      document.body.style.pointerEvents = '';
+    }
+    return () => {
+      document.body.style.pointerEvents = '';
+    };
+  }, [isOpen]);
+
   useOnClickOutside(modalRef, onClose);
 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
       <button
         onClick={onClose}
         className="absolute top-4 right-4 bg-black text-white p-2 rounded-full hover:border z-50"
@@ -18,7 +30,6 @@ const Modal = ({ isOpen, onClose, children }) => {
         <FaTimes className="w-6 h-6" />
       </button>
       <div ref={modalRef} className="relative w-full max-w-screen-lg bg-white rounded-lg shadow-lg">
-        {/* Close Button */}
         {children}
       </div>
     </div>
