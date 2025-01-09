58e7de9bee71dacf0ee7c457f84d47570f77e361
maldikurniawan <akurniawan2102@gmail.com>
2025-01-09 10:17:32 +0700

    fix aos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
   return (
     <div id="About" className="h-fit bg-white grid grid-cols-1 md:grid-cols-12 px-6 py-16 md:px-[80px] md:py-[5rem] gap-6">
-      <div className="col-span-full md:col-span-6" data-aos="fade-down-right" data-aos-delay="100">
+      <div className="col-span-full md:col-span-6" data-aos="fade-right" data-aos-delay="100">
         <div className="relative">
           <img src={aboutUs.image} alt="about" className="w-full rounded-xl" />
           <div className="absolute inset-0 flex items-center justify-center">
@@ -31,7 +31,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="col-span-full md:col-span-6" data-aos="fade-down-left" data-aos-delay="100">
+      <div className="col-span-full md:col-span-6" data-aos="fade-left" data-aos-delay="100">
         <div className="font-bold text-2xl sm:text-3xl md:text-4xl text-left mb-4 md:mb-10 text-green-600">
           {aboutUs.title}
         </div>
